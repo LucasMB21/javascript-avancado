1bddf4d52335ea8c611d0b2de71b9eeb8e1a09f0
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-04-13 18:12:12 -0300

    update

--- aluraframe/client/js/app/models/negociacao.js
+++ aluraframe/client/js/app/models/negociacao.js
@@ -2,7 +2,7 @@ class Negociacao {
 
     constructor(data, quantidade, valor) {
 
-        this._data = data;
+        this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
         Object.freeze(this);
@@ -14,7 +14,7 @@ class Negociacao {
     }
 
     get data() {
-        return this._data;
+        return new Date(this._data.getTime());
     }
 
     get quantidade() {
