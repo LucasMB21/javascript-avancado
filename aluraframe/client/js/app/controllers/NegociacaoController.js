94b34ced258d78e3767a85602c77f0e74d3204ea
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-04-14 13:57:45 -0300

    update NegociacaoController.js

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -15,11 +15,16 @@ class NegociacaoController {
         let data= new Date(...
             this._inputData.value
             .split('-')
-            .map(function(item, indice) {
-                return item - indice % 2
-            })
+            .map((item, indice) => item - indice % 2)
         );
-        console.log(data);
+
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
 
     }
 
