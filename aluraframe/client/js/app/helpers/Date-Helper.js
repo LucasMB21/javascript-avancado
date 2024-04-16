2b7b1d9222950f22b12d4106fa8d54b31d21220b
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-04-15 23:45:53 -0300

    update Date-Helper.js

--- aluraframe/client/js/app/helpers/Date-Helper.js
+++ aluraframe/client/js/app/helpers/Date-Helper.js
@@ -7,13 +7,15 @@ class DateHelper {
 
     static dataParaTexto(data) {
 
-        return data.getDate()
-        + '/' + (data.getMonth() + 1)
-        + '/' + data.getFullYear();
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
+
     }
 
     static textoParaData(texto) {
 
+       if(!/\d{4}-\d{2}-\d{2}/.test(texto)) 
+            throw new Error('deve estar no formato aaaa-mm-dd');
+        
        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
     }
 
