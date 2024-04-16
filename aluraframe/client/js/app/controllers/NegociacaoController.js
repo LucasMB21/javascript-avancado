bce818df537500e448c1bdc2379451c86f57ea25
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-04-15 23:23:22 -0300

    update Date-Helper.js

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -11,17 +11,15 @@ class NegociacaoController {
     adiciona(event) {
 
         event.preventDefault();
-
-        let helper = new DateHelper();
         
         let negociacao = new Negociacao(
-            helper.textoParaData(this._inputData.value),
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
 
         console.log(negociacao);
-        console.log(helper.dataParaTexto(negociacao.data));
+        console.log(DateHelper.dataParaTexto(negociacao.data));
 
     }
 
--- aluraframe/client/js/app/helpers/Date-Helper.js
+++ aluraframe/client/js/app/helpers/Date-Helper.js
@@ -1,13 +1,18 @@
 class DateHelper {
 
-    dataParaTexto(data) {
+    constructor() {
+
+        throw new Error('Esta classe não pode ser instanciada');
+    }
+
+    static dataParaTexto(data) {
 
         return data.getDate()
         + '/' + (data.getMonth() + 1)
         + '/' + data.getFullYear();
     }
 
-    textoParaData(texto) {
+    static textoParaData(texto) {
 
        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
     }
