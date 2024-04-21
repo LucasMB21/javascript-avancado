27279997349c651b93531df2ddba33eb2c6ecbff
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-04-20 22:39:51 -0300

    update NegociacoesView.js

--- aluraframe/client/js/app/views/NegociacoesView.js
+++ aluraframe/client/js/app/views/NegociacoesView.js
@@ -19,20 +19,25 @@ class NegociacoesView {
             </thead>
             
             <tbody>
-                ${model.negociacoes.map(n => {
-                    
-                    return `
-                        <tr>
-                            <td>${DateHelper.dataParaTexto(n.data)}</td>
-                            <td>${n.quantidade}</td>
-                            <td>${n.valor}</td>
-                            <td>${n.volume}</td>
-                        </t>
-                     `
-                }).join('')} 
+                ${model.negociacoes.map(n => `
+                    <tr>
+                        <td>${DateHelper.dataParaTexto(n.data)}</td>
+                        <td>${n.quantidade}</td>
+                        <td>${n.valor}</td>
+                        <td>${n.volume}</td>
+                    </tr>
+                `).join('')} 
             </tbody>
             
             <tfoot>
+                <td colspan="3"></td>
+                <td>${
+                    (function() {
+                        let total = 0;
+                        model.negociacoes.forEach(n => total+= n.volume);
+                        return total;
+                    })()
+                }</td>
             </tfoot>
         </table>
     
