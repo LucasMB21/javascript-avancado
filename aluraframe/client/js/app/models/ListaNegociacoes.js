490546b67ef56f2c6442bd0ee02b72c3eb9aa105
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-06-18 22:47:02 -0300

    update

--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,16 +1,13 @@
 class ListaNegociacoes {
     
-    constructor(armadilha) {
+    constructor() {
         
         this._negociacoes = [];
-        this._armadilha = armadilha;
     }
     
     adiciona(negociacao) {
         
         this._negociacoes.push(negociacao);
-        this._armadilha(this);
-        
     }
     
     get negociacoes() {
@@ -21,7 +18,6 @@ class ListaNegociacoes {
     esvazia() {
         
         this._negociacoes = [];
-        this._armadilha(this);
         
     }
 }
