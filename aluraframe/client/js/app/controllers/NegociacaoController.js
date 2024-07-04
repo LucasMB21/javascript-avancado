6c3bd65099f4746462e9baacd6912f1e10c2f3ad
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-07-03 23:08:26 -0300

    update

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -3,23 +3,19 @@ class NegociacaoController {
     constructor() {
         
         let $ = document.querySelector.bind(document);
+        
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-
         this._listaNegociacoes = new Bind(
             new ListaNegociacoes(),
-            this._negociacoesView,
-            ['adiciona', 'esvazia'])
-        
-        this._mensagemView = new MensagemView($('#mensagemView'));
+            new NegociacoesView($('#negociacoesView')),
+            'adiciona', 'esvazia');
 
         this._mensagem = new Bind(
-            new Mensagem(),
-            this._mensagemView,
-            ['texto']);
+            new Mensagem(), new MensagemView($('#mensagemView')),
+            'texto');
         
     }
     
