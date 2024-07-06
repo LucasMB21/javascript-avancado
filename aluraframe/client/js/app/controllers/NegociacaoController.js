c53467ac053f6a64b1fb3655ba58c58d36d1790d
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-07-06 19:41:05 -0300

    update NegociacaoController.js

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -28,11 +28,10 @@ class NegociacaoController {
     }
 
     importaNegociacoes() {
-
         let xhr = new XMLHttpRequest();
-
+    
         xhr.open('GET', 'negociacoes/semana');
-
+    
         xhr.onreadystatechange = () => {
 
             /*
@@ -46,23 +45,26 @@ class NegociacaoController {
 
             4: requisição concluída e a resposta está pronta
             */
+    
+            if (xhr.readyState == 4) {
 
-            if(xhr.readyState == 4) {
+                if (xhr.status == 200) {
 
-                if(xhr.status == 200) {
-
-                    console.log('Obtendo as negociações do servidor.');
+                    JSON.parse(xhr.responseText)
+                     .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                     .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                     this._mensagem.texto = 'Negociações importadas com sucesso';
 
                 } else {
-                    console.log('Não foi possível obter as negociações do servidor.');
+                    console.log(xhr.responseText);
+                    this._mensagem.texto = 'Não foi possível obter as negociações';
                 }
             }
-
         };
-
+    
         xhr.send();
     }
-
+    
     apaga() {
 
         this._listaNegociacoes.esvazia();
