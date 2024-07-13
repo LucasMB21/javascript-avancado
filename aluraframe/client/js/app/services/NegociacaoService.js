8b1775fd958f940d7f3a947ffae0b4a9952218b5
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-07-13 12:09:16 -0300

    update

--- aluraframe/client/js/app/services/NegociacaoService.js
+++ aluraframe/client/js/app/services/NegociacaoService.js
@@ -25,4 +25,53 @@ class NegociacaoService {
         xhr.send();
     }
 
+    obterNegociacoesDaSemanaAnterior(cb) {
+
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'negociacoes/anterior');
+
+        xhr.onreadystatechange = () => {
+
+            if (xhr.readyState == 4) {
+
+                if (xhr.status == 200) {
+
+                    cb(null, JSON.parse(xhr.responseText)
+                    .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+
+                } else {
+                    console.log(xhr.responseText);
+                    cb('Não foi possível obter as negociações', null);
+                }
+            }
+        };
+
+        xhr.send();
+    }
+
+    obterNegociacoesDaSemanaRetrasada(cb) {
+
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'negociacoes/retrasada');
+
+        xhr.onreadystatechange = () => {
+
+            if (xhr.readyState == 4) {
+
+                if (xhr.status == 200) {
+
+                    cb(null, JSON.parse(xhr.responseText)
+                    .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+
+                } else {
+                    console.log(xhr.responseText);
+                    cb('Não foi possível obter as negociações', null);
+                }
+            }
+        };
+
+        xhr.send();
+    }
 }
